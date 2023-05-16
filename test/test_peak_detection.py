06ae219083cf4c7625c524b2717ccff7cbcd4787
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-05-16 10:48:34 +0200

    test: update detect peaks test

--- test/test_peak_detection.py
+++ test/test_peak_detection.py
@@ -1,30 +1,28 @@
 import pytest
 
 import ntt_peaks
-from ntt_peaks import Signal
 from ntt_peaks.data import load_example_data
-from ntt_peaks.util import _get_all_algorithms
+
+algorithms = ["neighbours_and_threshold"]
 
 
 @pytest.mark.parametrize("data,n_peaks_expected", [([0, 0, 5, 0, 0], 1)])
 @pytest.mark.parametrize(
     "peak_detection_algorithm",
-    list(_get_all_algorithms()),
+    algorithms,
 )
 def test_peak_detection_synthetic_data(
     data, n_peaks_expected, peak_detection_algorithm
 ):
     data = [0, 0, 5, 0, 0]
-    peak_idxs: list[int] = ntt_peaks.detect_peaks(
-        Signal(x=list(range(len(data))), y=data), peak_detection_algorithm
-    )
+    peak_idxs = ntt_peaks.detect_peaks(data, peak_detection_algorithm)
     assert isinstance(peak_idxs, list)
     assert len(peak_idxs) == n_peaks_expected
 
 
 @pytest.mark.parametrize(
     "peak_detection_algorithm",
-    list(_get_all_algorithms()),
+    algorithms,
 )
 @pytest.mark.parametrize(
     "data",
@@ -33,5 +31,5 @@ def test_peak_detection_synthetic_data(
 )
 def test_peak_detection_real_data(data, peak_detection_algorithm):
     """Test peak detection algorithms detect expected number of peaks."""
-    peaks = ntt_peaks.detect_peaks(data, peak_detection_algorithm)
+    peaks = ntt_peaks.detect_peaks(data.y, peak_detection_algorithm)
     assert len(peaks) == 2
