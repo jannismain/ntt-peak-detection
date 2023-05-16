6c55b22e9d5ceff08fd4df425592899484a9a911
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-05-16 11:51:41 +0200

    fix: return type hint of peak detection methods

--- src/ntt_peaks/lib.py
+++ src/ntt_peaks/lib.py
@@ -5,7 +5,7 @@ import numpy as np
 from . import Signal
 
 
-def neighbours(values: list[int | float]) -> Signal:
+def neighbours(values: list[int | float]) -> list[int]:
     """Detect peak by comparing values to left and right neighbours.
 
     A peak is detected if the value is greater than its left and right neighbours.
@@ -17,13 +17,13 @@ def neighbours(values: list[int | float]) -> Signal:
     ]
 
 
-def threshold(values: list[int | float]) -> Signal:
+def threshold(values: list[int | float]) -> list[int]:
     """Detect a peak by comparing values to a threshold value."""
     threshold = np.mean(values) * 2
     return [idx for idx in range(len(values)) if values[idx] > threshold]
 
 
-def neighbours_and_threshold(values: list[int | float]) -> Signal:
+def neighbours_and_threshold(values: list[int | float]) -> list[int]:
     """Detect a peak by comparing values to a threshold value and neighbours."""
     return list(set(neighbours(values)) & set(threshold(values)))
 
