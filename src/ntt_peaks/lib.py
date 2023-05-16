661c2335451643ac85a6db75e3855c0fb80febe8
Jannis Mainczyk <jannis.mainczyk@iis.fraunhofer.de>
2023-05-16 10:22:09 +0200

    refactor: detect_peaks doesn't depend on Signal
    as only y values are used for peak detection, a simple list of
    integers or floats suffices

--- src/ntt_peaks/lib.py
+++ src/ntt_peaks/lib.py
@@ -3,35 +3,37 @@ import numpy as np
 from . import Signal
 
 
-def neighbours(s: Signal) -> Signal:
+def neighbours(values: list[int | float]) -> Signal:
     """Detect peak by comparing values to left and right neighbours.
 
     A peak is detected if the value is greater than its left and right neighbours.
     """
     return [
-        idx for idx in range(1, len(s.y) - 1) if s.y[idx - 1] < s.y[idx] > s.y[idx + 1]
+        idx
+        for idx in range(1, len(values) - 1)
+        if values[idx - 1] < values[idx] > values[idx + 1]
     ]
 
 
-def threshold(s: Signal) -> Signal:
+def threshold(values: list[int | float]) -> Signal:
     """Detect a peak by comparing values to a threshold value."""
-    threshold = np.mean(s.y) * 2
-    return [idx for idx in range(len(s.y)) if s.y[idx] > threshold]
+    threshold = np.mean(values) * 2
+    return [idx for idx in range(len(values)) if values[idx] > threshold]
 
 
-def neighbours_and_threshold(s: Signal) -> Signal:
+def neighbours_and_threshold(values: list[int | float]) -> Signal:
     """Detect a peak by comparing values to a threshold value and neighbours."""
-    return set(neighbours(s)) & set(threshold(s))
+    return set(neighbours(values)) & set(threshold(values))
 
 
-def scipy_regular(s: Signal) -> list[int]:
+def scipy_regular(values: list[int | float]) -> list[int]:
     import scipy
 
-    peaks, _ = scipy.signal.find_peaks(s.y)
+    peaks, _ = scipy.signal.find_peaks(values)
     return peaks
 
 
-def scipy_cwt(s: Signal) -> list[int]:
+def scipy_cwt(values: list[int | float]) -> list[int]:
     import scipy
 
-    return scipy.signal.find_peaks_cwt(s.y, widths=[1, 2, 3, 4, 5])
+    return scipy.signal.find_peaks_cwt(values, widths=[1, 2, 3, 4, 5])
--- src/ntt_peaks/util.py
+++ src/ntt_peaks/util.py
@@ -4,9 +4,9 @@ from types import FunctionType
 from . import Signal
 
 
-def detect_peaks(s: Signal, algorithm: str = "neighbours") -> list[int]:
+def detect_peaks(v: list[int | float], algorithm: str = "neighbours") -> list[int]:
     """Detect peaks in given signal and return their indices."""
-    return _get_all_algorithms()[algorithm](s)
+    return _get_all_algorithms()[algorithm](v)
 
 
 def _get_all_algorithms() -> dict[str, FunctionType]:
